b8a463732153c296205d63d3cf7aeefb1d242ce1
deepa_kashyap <deepa494671@gmail.com>
2025-07-25 16:02:49 +0530

    .

--- Frontend/TO-Do/src/main.jsx
+++ Frontend/TO-Do/src/main.jsx
@@ -8,12 +8,12 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    
      <ThemeProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </ThemeProvider>
-  </BrowserRouter>
+
     </StrictMode>
 );
