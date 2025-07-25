6c9705f7e915ea3efff4bfff1799f68225e6b188
deepa_kashyap <deepa494671@gmail.com>
2025-07-25 15:49:06 +0530

    ,

--- Frontend/TO-Do/src/main.jsx
+++ Frontend/TO-Do/src/main.jsx
@@ -4,16 +4,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "../context/AuthContext";
 import { ThemeProvider } from "../context/ThemeContext";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <ThemeProvider>
         <AuthProvider>
           <App />
         </AuthProvider>
       </ThemeProvider>
-    </BrowserRouter>
-  </StrictMode>
+    </Router>
+    </StrictMode>
 );
